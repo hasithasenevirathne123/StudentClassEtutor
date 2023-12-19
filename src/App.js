43b8f168107254c2e132fcf381396859ac2a1918
hasithasenevirathne123 <44221373+hasithasenevirathne123@users.noreply.github.com>
2023-12-19 15:55:36 +0530

    Radio buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Typography, Button, IconButton, Stack, TextField, InputAdornment } from '@mui/material';
+import { Typography, Button, IconButton, Stack, TextField, InputAdornment, Radio } from '@mui/material';
 import AddToHomeScreenIcon from '@mui/icons-material/AddToHomeScreen';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import AddLocationIcon from '@mui/icons-material/AddLocation';
@@ -10,7 +10,6 @@ function App() {
   const [data, setData]= useState('')
   return (
     <div className="App">
-
       {/* Typography */}
       {/* <header className="App-header">
         <p>
@@ -21,20 +20,15 @@ function App() {
           <Typography variant='body1'>Hello world</Typography>
           <Typography variant='body2'>Hello world</Typography>
           <Typography variant='caption'>Caption</Typography>
-
-    
         </p>
       </header> */}
-
-
      {/*Buttons */}
-      <Button variant='text' href='mui.com' size='small'>Text</Button>
+      {/* <Button variant='text' href='mui.com' size='small'>Text</Button>
       <Button variant='contained' size='large' color='secondary'>Contained</Button>
-      <Button variant='outlined' color='success'>outlined</Button>
+      <Button variant='outlined' color='success'>outlined</Button> */}
+      {/* <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
 
-      <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
-
-        {/* <Button variant='contained' size='large' color='primary' onClick={()=>{alert('added')}} startIcon={<AddCircleIcon/>}>Add Contact</Button>
+        <Button variant='contained' size='large' color='primary' onClick={()=>{alert('added')}} startIcon={<AddCircleIcon/>}>Add Contact</Button>
         <Button variant='contained' size='large' color='success' startIcon={<AddToHomeScreenIcon/>}>HomePage</Button>
         <Button variant='contained' size='large' color='secondary' startIcon={<AddLocationIcon/>}>AddLocation</Button>
 
@@ -42,15 +36,14 @@ function App() {
         
         <AddLocationIcon/>
 
-        </IconButton> */}
+        </IconButton>
 
         <TextField label="outlined" variant='outlined' color='success' size='large' required/>
         <TextField label="filled" variant='filled' />
         <TextField label="standard" variant='standard' helperText='Add something' type='password'/>
 
-      </Stack>
-
-      <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
+      </Stack> */}
+      {/* <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
       
       <TextField label='Money' variant='outlined' InputProps={{
         endAdornment: <InputAdornment position='start'><AddLocationIcon/></InputAdornment>
@@ -63,7 +56,29 @@ function App() {
        error = {!data}
        helperText= {!data ? 'required': 'done'}
        variant='outlined'/>
-      </Stack>
+      </Stack> */}
+
+
+<Stack direction='row' spacing={2} justifyContent='center' margin={5}>
+
+<Radio
+ value= 'a'
+ onChange={e=>setData(e.target.value)}
+ checked={data === 'a'}/>
+
+<Radio
+ value= 'b'
+ onChange={e=>setData(e.target.value)}
+ checked={data === 'b'}/>
+
+</Stack>
+
+<Stack direction='row' spacing={2} justifyContent='center' margin={5}>
+{data}
+</Stack>
+
+
+
 
     </div>
   );
