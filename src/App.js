7303cd0978e013a11bbf42f9b15cbd69dfd8a32b
hasithasenevirathne123 <44221373+hasithasenevirathne123@users.noreply.github.com>
2023-12-19 15:15:49 +0530

    TextField Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Typography, Button } from '@mui/material';
+import { Typography, Button, IconButton, Stack, TextField, InputAdornment } from '@mui/material';
 import AddToHomeScreenIcon from '@mui/icons-material/AddToHomeScreen';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import AddLocationIcon from '@mui/icons-material/AddLocation';
@@ -30,11 +30,31 @@ function App() {
       <Button variant='contained' size='large' color='secondary'>Contained</Button>
       <Button variant='outlined' color='success'>outlined</Button>
 
-      <div className='add-btn'>
-        <Button variant='contained' size='large' color='primary' startIcon={<AddCircleIcon/>}>Add Contact</Button>
+      <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
+
+        {/* <Button variant='contained' size='large' color='primary' onClick={()=>{alert('added')}} startIcon={<AddCircleIcon/>}>Add Contact</Button>
         <Button variant='contained' size='large' color='success' startIcon={<AddToHomeScreenIcon/>}>HomePage</Button>
         <Button variant='contained' size='large' color='secondary' startIcon={<AddLocationIcon/>}>AddLocation</Button>
-      </div>
+
+        <IconButton aria-label='add-location' onClick={()=>{alert('location added')}} size='large' color='error'>
+        
+        <AddLocationIcon/>
+
+        </IconButton> */}
+
+        <TextField label="outlined" variant='outlined' color='success' size='large' required/>
+        <TextField label="filled" variant='filled' />
+        <TextField label="standard" variant='standard' helperText='Add something' type='password'/>
+
+      </Stack>
+
+      <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
+      
+      <TextField label='size' variant='outlined' InputProps={{
+        startAdornment: <InputAdornment position='start'>H</InputAdornment>
+      }}/>
+      </Stack>
+
     </div>
   );
 }
