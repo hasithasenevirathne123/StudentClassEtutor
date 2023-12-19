0f08880c0a77b96bd501c0683d1fa4dc98434820
hasithasenevirathne123 <44221373+hasithasenevirathne123@users.noreply.github.com>
2023-12-19 12:50:30 +0530

    React matrial UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Typography, Button } from '@mui/material';
+import AddToHomeScreenIcon from '@mui/icons-material/AddToHomeScreen';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import AddLocationIcon from '@mui/icons-material/AddLocation';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+      {/* Typography */}
+      {/* <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Typography color='primary' gutterBottom variant='h1'>Hello world</Typography>
+          <Typography color='secondary' gutterBottom variant='h2'>Hello world</Typography>
+          <Typography color='red' variant='h3'>Hello world</Typography>
+          <Typography variant='h4'>Hello world</Typography>
+          <Typography variant='body1'>Hello world</Typography>
+          <Typography variant='body2'>Hello world</Typography>
+          <Typography variant='caption'>Caption</Typography>
+
+    
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </header> */}
+
+
+     {/*Buttons */}
+      <Button variant='text' href='mui.com' size='small'>Text</Button>
+      <Button variant='contained' size='large' color='secondary'>Contained</Button>
+      <Button variant='outlined' color='success'>outlined</Button>
+
+      <div className='add-btn'>
+        <Button variant='contained' size='large' color='primary' startIcon={<AddCircleIcon/>}>Add Contact</Button>
+        <Button variant='contained' size='large' color='success' startIcon={<AddToHomeScreenIcon/>}>HomePage</Button>
+        <Button variant='contained' size='large' color='secondary' startIcon={<AddLocationIcon/>}>AddLocation</Button>
+      </div>
     </div>
   );
 }
