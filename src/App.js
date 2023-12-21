4d16548de405d0ab1241ce0f2c72e510cc593206
hasithasenevirathne123 <44221373+hasithasenevirathne123@users.noreply.github.com>
2023-12-21 15:00:11 +0530

    RadioGroup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { Typography, Button, IconButton, Stack, TextField, InputAdornment, Radio } from '@mui/material';
+import { Typography, Button, IconButton, Stack, TextField, InputAdornment, Radio, RadioGroup, FormControl, FormLabel, FormControlLabel } from '@mui/material';
 import AddToHomeScreenIcon from '@mui/icons-material/AddToHomeScreen';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import AddLocationIcon from '@mui/icons-material/AddLocation';
@@ -9,79 +9,49 @@ import { useState } from 'react';
 function App() {
   const [data, setData]= useState('')
   return (
-    <div className="App">
-      {/* Typography */}
+  <div className="App">
+    <Stack direction="row" spacing={2} justifyContent="center" margin={5}>
 
-      <header className="App-header">
-        <p>
-          <Typography color='primary' gutterBottom variant='h1'>Hello world</Typography>
-          <Typography color='secondary' gutterBottom variant='h2'>Hello world</Typography>
-          <Typography color='red' variant='h3'>Hello world</Typography>
-          <Typography variant='h4'>Hello world</Typography>
-          <Typography variant='body1'>Hello world</Typography>
-          <Typography variant='body2'>Hello world</Typography>
-          <Typography variant='caption'>Caption</Typography>
-        </p>
-      </header>
-     {/*Buttons */}
-      <Button variant='text' href='mui.com' size='small'>Text</Button>
-      <Button variant='contained' size='large' color='secondary'>Contained</Button>
-      <Button variant='outlined' color='success'>outlined</Button>
-      <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
+    <FormControl>
 
-        <Button variant='contained' size='large' color='primary' onClick={()=>{alert('added')}} startIcon={<AddCircleIcon/>}>Add Contact</Button>
-        <Button variant='contained' size='large' color='success' startIcon={<AddToHomeScreenIcon/>}>HomePage</Button>
-        <Button variant='contained' size='large' color='secondary' startIcon={<AddLocationIcon/>}>AddLocation</Button>
-
-        <IconButton aria-label='add-location' onClick={()=>{alert('location added')}} size='large' color='error'>
-        
-        <AddLocationIcon/>
-
-        </IconButton>
-
-        <TextField label="outlined" variant='outlined' color='success' size='large' required/>
-        <TextField label="filled" variant='filled' />
-        <TextField label="standard" variant='standard' helperText='Add something' type='password'/>
-
-      </Stack>
-      <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
+    <FormLabel id='pl'>Programming Languages</FormLabel>
+      <RadioGroup
+      name='Programming language to learn'
+      aria-labelledby='Programming_language_to_learn'
+      value={data}
+      onChange={e => setData(e.target.value)}
+      row
+      >
+      <FormControlLabel
+      value="C Programming"
+      control={<Radio/>}
+      label="C Programming"
+      />
+     <FormControlLabel
+      value="JAVA"
+      control={<Radio/>}
+      label="JAVA"
+      />
+     <FormControlLabel
+      value="Phython"
+      control={<Radio/>}
+      label="python"
+      />
+     <FormControlLabel
+      value="Python"
+      control={<Radio/>}
+      label="Python"
+      />
+      </RadioGroup>
       
-      <TextField label='Money' variant='outlined' InputProps={{
-        endAdornment: <InputAdornment position='start'><AddLocationIcon/></InputAdornment>
-      }}/>
+    </FormControl>
 
-      <TextField
-       label="input"
-       value={data}
-       onChange={e=>setData(e.target.value)}
-       error = {!data}
-       helperText= {!data ? 'required': 'done'}
-       variant='outlined'/>
-      </Stack>
+    </Stack>
 
-
-<Stack direction='row' spacing={2} justifyContent='center' margin={5}>
-
-<Radio
- value= 'a'
- onChange={e=>setData(e.target.value)}
- checked={data === 'a'}/>
-
-<Radio
- value= 'b'
- onChange={e=>setData(e.target.value)}
- checked={data === 'b'}/>
-
-</Stack>
-
-<Stack direction='row' spacing={2} justifyContent='center' margin={5}>
-{data}
-</Stack>
-
-
-
-
-    </div>
+    <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
+      {data}
+    </Stack>
+</div>
   );
 }
 
