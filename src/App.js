ee4754d7e2201c56b16a50133cc68a4f8b2d025f
hasithasenevirathne123 <44221373+hasithasenevirathne123@users.noreply.github.com>
2023-12-21 15:01:32 +0530

    Radiogroup update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,60 @@ function App() {
   const [data, setData]= useState('')
   return (
   <div className="App">
+     {/* Typography */}
+
+      {/* <header className="App-header">
+        <p>
+          <Typography color='primary' gutterBottom variant='h1'>Hello world</Typography>
+          <Typography color='secondary' gutterBottom variant='h2'>Hello world</Typography>
+          <Typography color='red' variant='h3'>Hello world</Typography>
+          <Typography variant='h4'>Hello world</Typography>
+          <Typography variant='body1'>Hello world</Typography>
+          <Typography variant='body2'>Hello world</Typography>
+          <Typography variant='caption'>Caption</Typography>
+        </p>
+      </header> */}
+
+     {/*Buttons */}
+
+      {/* <Button variant='text' href='mui.com' size='small'>Text</Button>
+      <Button variant='contained' size='large' color='secondary'>Contained</Button>
+      <Button variant='outlined' color='success'>outlined</Button> */}
+
+      {/* <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
+
+        <Button variant='contained' size='large' color='primary' onClick={()=>{alert('added')}} startIcon={<AddCircleIcon/>}>Add Contact</Button>
+        <Button variant='contained' size='large' color='success' startIcon={<AddToHomeScreenIcon/>}>HomePage</Button>
+        <Button variant='contained' size='large' color='secondary' startIcon={<AddLocationIcon/>}>AddLocation</Button>
+
+        <IconButton aria-label='add-location' onClick={()=>{alert('location added')}} size='large' color='error'>
+        
+        <AddLocationIcon/>
+
+        </IconButton>
+
+        <TextField label="outlined" variant='outlined' color='success' size='large' required/>
+        <TextField label="filled" variant='filled' />
+        <TextField label="standard" variant='standard' helperText='Add something' type='password'/>
+
+      </Stack> */}
+
+      {/* <Stack direction='row' spacing={2} justifyContent='center' margin={5}>
+      
+      <TextField label='Money' variant='outlined' InputProps={{
+        endAdornment: <InputAdornment position='start'><AddLocationIcon/></InputAdornment>
+      }}/>
+
+      <TextField
+       label="input"
+       value={data}
+       onChange={e=>setData(e.target.value)}
+       error = {!data}
+       helperText= {!data ? 'required': 'done'}
+       variant='outlined'/>
+      </Stack> */}
+
+
     <Stack direction="row" spacing={2} justifyContent="center" margin={5}>
 
     <FormControl>
@@ -43,7 +97,7 @@ function App() {
       label="Python"
       />
       </RadioGroup>
-      
+
     </FormControl>
 
     </Stack>
